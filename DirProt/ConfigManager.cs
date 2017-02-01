bdf7c1019b0c956b6eace848a5666e21461ea991
AlecJY <ae40515@yahoo.com.tw>
2017-02-01 22:47:06 +0800

    Add project files.

---
+++ DirProt/ConfigManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace DirProt {
+    public class ConfigManager {
+        public Config LoadConfig(string filename) {
+            string jsonString = "";
+            try {
+                jsonString = File.ReadAllText(filename);
+            } catch (Exception) {
+                Console.Error.WriteLine("Config file not found");
+                Environment.Exit(-1);
+            }
+            try {
+                return JsonConvert.DeserializeObject<Config>(jsonString);
+            } catch (JsonReaderException e) {
+                Console.Error.WriteLine("Cannot parse config file");
+                Console.Error.WriteLine(e);
+                Environment.Exit(-1);
+                return null;
+            }
+        }
+
+        public DirTable LoadDirTable(string filename) {
+            string jsonString = "";
+            try {
+                jsonString = File.ReadAllText(filename);
+            } catch (Exception) {
+                Console.WriteLine("Dir table not found\nCreate new dir table");
+            }
+            try {
+                DirTable dirTable = JsonConvert.DeserializeObject<DirTable>(jsonString);
+                if (dirTable != null) {
+                    return dirTable;
+                } else {
+                    return new DirTable();
+                }
+            } catch (JsonReaderException e) {
+                Console.Error.WriteLine("Cannot parse config file");
+                Console.Error.WriteLine(e);
+                Environment.Exit(-1);
+                return null;
+            }
+        }
+
+        public void SaveDirectorys(List<DirPath> directorys, string path) {
+            DirTable dirTable = new DirTable();
+            dirTable.Directorys = directorys;
+            string jsonString = JsonConvert.SerializeObject(dirTable);
+            try {
+                File.WriteAllText(path, jsonString);
+            }
+            catch (Exception e) {
+                Console.Error.WriteLine(e);
+            }
+        }
+    }
+
+    public class Config {
+        public bool Enabled { get; set; }
+        public List<string> ProtectedDir { get; set; } = new List<string>();
+    }
+
+    public class DirTable {
+        public List<DirPath> Directorys { get; set; } = new List<DirPath>();
+    }
+
+    public class DirPath {
+        public string path { get; set; } = "";
+        public string hash { get; set; } = "";
+        public uint index { get; set; }
+    }
+}
---
+++ DirProt/DirProt.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DirProt {
+    class DirProt {
+        private static readonly string AppDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) +
+                                         Path.DirectorySeparatorChar;
+
+        public DirProt() {
+            ConfigManager configManager = new ConfigManager();
+            Config config = configManager.LoadConfig(AppDir + "config.json");
+            List<DirPath> directorys = configManager.LoadDirTable(AppDir + "data" + Path.DirectorySeparatorChar + "dirtable.json").Directorys;
+            if (config.Enabled) {
+                foreach (string path in config.ProtectedDir) {
+                    bool isBackuped = false;
+                    DirPath backupDir = null;
+                    foreach (DirPath directory in directorys) {
+                        if (path.Equals(directory.path)) {
+                            isBackuped = true;
+                            backupDir = directory;
+                        }
+                    }
+                    if (isBackuped) {
+                        DirectoryInfo directoryInfo = new DirectoryInfo(path);
+                        if (directoryInfo.Exists) {
+                            foreach (FileSystemInfo childInfo in directoryInfo.GetFileSystemInfos()) {
+                                DeleteReadOnly(childInfo);
+                            }
+                        }
+                        CopyDirectory(AppDir + Path.DirectorySeparatorChar + "data" + Path.DirectorySeparatorChar + backupDir.hash + Path.DirectorySeparatorChar + backupDir.index, path);
+                    } else {
+                        Backup(path, directorys);
+                    }
+                }
+                configManager.SaveDirectorys(directorys, AppDir + Path.DirectorySeparatorChar + "data" + Path.DirectorySeparatorChar + "dirtable.json");
+            }
+        }
+
+        public void Backup(string path, List<DirPath> directorys) {
+            SHA256 sha256 = new SHA256CryptoServiceProvider();
+            SHA1 sha1 = new SHA1CryptoServiceProvider();
+            byte[] hashBytes = sha1.ComputeHash(Encoding.UTF8.GetBytes(path));
+            string hash = BitConverter.ToString(hashBytes).Replace("-", string.Empty).ToLower();
+            uint index = 0;
+            foreach (DirPath directory in directorys) {
+                if (hash.Equals(directory.hash)) {
+                    index++;
+                }
+            }
+            DirPath dirPath = new DirPath();
+            dirPath.path = path;
+            dirPath.hash = hash;
+            dirPath.index = index;
+            directorys.Add(dirPath);
+            CopyDirectory(path, AppDir + "data" + Path.DirectorySeparatorChar + hash + Path.DirectorySeparatorChar + index);
+        }
+
+        private void CopyDirectory(string srcPath, string dstPath) {
+            if (Directory.Exists(srcPath)) {
+                if (!Directory.Exists(dstPath)) {
+                    Directory.CreateDirectory(dstPath);
+                }
+                DirectoryInfo srcDir = new DirectoryInfo(srcPath);
+                foreach (FileInfo file in srcDir.GetFiles()) {
+                    try {
+                        FileInfo dstFile = new FileInfo(dstPath + Path.DirectorySeparatorChar + file.Name);
+                        if (dstFile.Exists) {
+                            dstFile.Attributes = FileAttributes.Normal;
+                        }
+                        file.CopyTo(dstPath + Path.DirectorySeparatorChar + file.Name, true);
+                    }
+                    catch (Exception e) {
+                        Console.Error.WriteLine(e);
+                    } 
+                }
+                foreach (DirectoryInfo directory in srcDir.GetDirectories()) {
+                    try {
+                        DirectoryInfo dstSubDir =
+                            new DirectoryInfo(dstPath + Path.DirectorySeparatorChar + directory.Name);
+                        dstSubDir.Create();
+                        dstSubDir.Attributes = directory.Attributes;
+                        CopyDirectory(directory.FullName, dstSubDir.FullName);
+                    }
+                    catch (Exception e) {
+                        Console.Error.WriteLine(e);
+                    }
+                }
+            }
+            else {
+                Console.Error.WriteLine("Directory not found: " + srcPath);
+            }
+        }
+
+        private void DeleteReadOnly(FileSystemInfo fileSystemInfo) {
+            var directoryInfo = fileSystemInfo as DirectoryInfo;
+            if (directoryInfo != null && directoryInfo.Exists) {
+                foreach (FileSystemInfo childInfo in directoryInfo.GetFileSystemInfos()) {
+                    DeleteReadOnly(childInfo);
+                }
+            }
+            try {
+                fileSystemInfo.Attributes = FileAttributes.Normal;
+                fileSystemInfo.Delete();
+            }
+            catch (Exception e) {
+                Console.Error.WriteLine(e);
+            }
+        }
+
+        static void Main(string[] args) {
+            new DirProt();
+        }
+    }
+}
